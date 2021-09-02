93939ec3af0f3aeac4f6430932bb819ba26602bb
JaznanOfficial <jaznanofficial@gmail.com>
2021-09-02 10:47:18 +0600

    first commit after a long time i was mad

---
+++ main.js
@@ -0,0 +1,31 @@
+const searchBook = () => {
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+
+    // searchField.value = '';
+
+    const url = `https://openlibrary.org/search.json?q=${searchText}`
+    fetch(url)
+        .then(response => response.json())
+        .then(data => displaySearchResult(data.docs))
+}
+
+const displaySearchResult = docs => {
+    const searchResult = document.getElementById('search-result');
+    docs.forEach(doc => {
+        console.log(doc);
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+        <div class="card h-100">
+            <img src="..." class="card-img-top" alt="..." />
+            <div class="card-body">
+                <h4 class="card-title">${doc.title}</h4>
+                <h6>Author: ${doc.author_name}</h6>
+                <p><b>Published Year: ${doc.publish_date[0]}</b></p>
+            </div>
+        </div>
+        `;
+        searchResult.appendChild(div);
+    });
+}
