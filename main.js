eec43127e73c8d3d173e424c004785ab24ff76c7
JaznanOfficial <jaznanofficial@gmail.com>
2021-09-02 11:42:15 +0600

    completeted but need some style

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const searchBook = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
 
-    // searchField.value = '';
+    searchField.value = '';
 
     const url = `https://openlibrary.org/search.json?q=${searchText}`
     fetch(url)
@@ -17,12 +17,15 @@ const displaySearchResult = docs => {
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
-        <div class="card h-100">
-            <img src="..." class="card-img-top" alt="..." />
+        <div class="card h-100 text-center">
+            <div class="h-75">
+            <img src="https://covers.openlibrary.org/b/id/${doc.cover_i}-M.jpg" class="card-img-top h-100" alt="This Picture Is not in api" />
+            </div
             <div class="card-body">
                 <h4 class="card-title">${doc.title}</h4>
                 <h6>Author: ${doc.author_name}</h6>
                 <p><b>Published Date: ${doc.publish_date[0]}</b></p>
+                <p><b>Publisher: ${doc.publisher[0]}</b></p>
             </div>
         </div>
         `;
